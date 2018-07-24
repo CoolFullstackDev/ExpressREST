bacc05938d56cb6c808e48006ad89de8cc36a05d
CoolFullstackDev <topace1029@hotmail.com>
2018-07-24 01:37:44 -0700

    mongodb install commit

--- express_backend/app.js
+++ express_backend/app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', function(err){
+    console.log(err);
+});
 
 //Init App
 const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
+
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -10,32 +28,18 @@ app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', function(req, res){
-    let articles = [
-        {
-            id: 1,
-            title: 'Article One',
-            author: 'Brad Traversy',
-            body: 'This is article  one'
-        },
-        {
-            id: 2,
-            title: 'Article two',
-            author: 'Brad Traversy',
-            body: 'This is article  two'
-        },
-        {
-            id: 3,
-            title: 'Article three',
-            author: 'Brad Traversy',
-            body: 'This is article  three'
-        },
-
-    ];
-
-    res.render('index', {
-        title: 'Articles',
-        articles: articles
+    Article.find({}, function(err, articles){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
+        }
+        
     });
+    
 });
 
 // Add Route
